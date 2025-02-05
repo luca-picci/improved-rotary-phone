2352b2aa7769c2e49551274491e8f99cd76cf073
Luca Picci <lpicci@outlook.com>
2025-02-05 14:27:55 +0100

    Delete Desktop directory

--- Desktop/improved-rotary-phone/src/app/models/user.model.ts
+++
@@ -1,7 +0,0 @@
-export interface User {
-  id: number;
-  name: string;
-  email: string;
-  password?: string; // Il campo password è opzionale per motivi di sicurezza
-  userType: 'CLIENT' | 'MANAGER';
-}
--- Desktop/improved-rotary-phone/src/app/models/venue.model.ts
+++
@@ -1,6 +0,0 @@
-export interface Venue {
-  id: number;
-  name: string;
-  address: string;
-  description: string;
-}
